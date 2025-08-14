ff7ea308e170f5538a677a72409017261ba1ecff
Michael <m.a.piryazev@gmail.com>
2025-08-14 11:41:45 +0300

    consumer

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/MAPiryazev/Wildberries_L0/internal/db"
 	"github.com/MAPiryazev/Wildberries_L0/internal/handlers"
+	"github.com/MAPiryazev/Wildberries_L0/internal/kafka"
 	"github.com/MAPiryazev/Wildberries_L0/internal/repository"
 	"github.com/MAPiryazev/Wildberries_L0/internal/service"
 )
@@ -22,6 +24,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	//TODO  вот это все в env запихнуть
+	consumer := kafka.NewOrderConsumer("localhost:29092", "orders", "order-consumer-group", orderService)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	go consumer.Start(ctx)
 
 	handler := handlers.NewHandler(orderService)
 	router := handlers.RoutesInit(handler)
--- internal/kafka/consumer.go
+++ internal/kafka/consumer.go
@@ -1,1 +1,65 @@
 package kafka
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+
+	models "github.com/MAPiryazev/Wildberries_L0/internal/model"
+	"github.com/MAPiryazev/Wildberries_L0/internal/service"
+	"github.com/segmentio/kafka-go"
+)
+
+type OrderConsumer struct {
+	kafkaBroker string
+	topic       string
+	groupID     string
+	orderSvc    service.OrderService
+}
+
+func NewOrderConsumer(broker, topic, groupID string, svc service.OrderService) *OrderConsumer {
+	return &OrderConsumer{
+		kafkaBroker: broker,
+		topic:       topic,
+		groupID:     groupID,
+		orderSvc:    svc,
+	}
+}
+
+func (consumer *OrderConsumer) Start(ctx context.Context) {
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{consumer.kafkaBroker},
+		GroupID: consumer.groupID,
+		Topic:   consumer.topic,
+	})
+	defer reader.Close()
+
+	log.Println("Консъюмер кафки запущен")
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("Консьюмер кафки остановлен")
+			return
+		default:
+			message, err := reader.ReadMessage(ctx)
+			if err != nil {
+				log.Printf("Ошибка чтения сообщения: %v", err)
+				continue
+			}
+
+			var order models.Order
+			if err := json.Unmarshal(message.Value, &order); err != nil {
+				log.Printf("Ошибка десериализации json сообщения: %v", err)
+				continue
+			}
+
+			if err := consumer.orderSvc.SaveOrder(&order); err != nil {
+				log.Printf("Ошибка при сохранении заказа в БД %s: %v", order.OrderUID, err)
+				continue
+			}
+
+			log.Printf("Заказ %s успешно сохранен в БД", order.OrderUID)
+		}
+	}
+}
